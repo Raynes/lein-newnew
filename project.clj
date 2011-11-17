8a2c73d9b87faba34a39f265c3bb881f19f8c971
Anthony Grimes <i@raynes.me>
2011-11-17 13:13:09 -0600

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-newnew "0.1.1"
+(defproject lein-newnew "0.1.2"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [stencil "0.2.0"]]
