17eef0aa870fddd48f76c211ea9d98d2e4181040
Anthony Grimes <i@raynes.me>
2011-11-16 20:44:08 -0600

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-newnew "1.0.0-SNAPSHOT"
+(defproject lein-newnew "0.1.0"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.rayne/clostache "0.7.0-SNAPSHOT"]]
