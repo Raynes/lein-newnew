09013d18db27fe421dc241e81fbbd77e9c288b36
Anthony Grimes <i@raynes.me>
2011-11-16 21:36:07 -0600

    Use 1.2.1, since Leiningen doesn't do 1.3.0 yet.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-newnew "0.1.0"
   :description "A Leiningen plugin for generating new projects based on templates."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojars.rayne/clostache "0.7.0-SNAPSHOT"]]
   :eval-in-leiningen true)
