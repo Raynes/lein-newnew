bfda501ed011fe6be4ff36560d1bc418876500ac
Anthony Grimes <i@raynes.me>
2012-02-05 01:28:11 -0600

    Get rid of pointless clojure dep.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject lein-newnew "0.1.2"
   :description "A Leiningen plugin for generating new projects based on templates."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [stencil "0.2.0"]]
+  :dependencies [[stencil "0.2.0"]]
   :eval-in-leiningen true)
