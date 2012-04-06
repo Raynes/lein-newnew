480587d3a9e5e52bc4f7086ea87c0ef0e1292e61
Phil Hagelberg <technomancy@gmail.com>
2012-04-06 13:50:05 -0700

    Bump to 0.2.6-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-newnew "0.2.5"
+(defproject lein-newnew "0.2.6-SNAPSHOT"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.2.0"]
-                 [bultitude "0.1.3"]]
+                 [bultitude "0.1.4"]]
   :eval-in-leiningen true)
