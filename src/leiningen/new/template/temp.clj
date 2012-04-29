864e86f7aa93298497da7d972318d735f0b91f6d
Anthony Grimes <i@raynes.me>
2012-04-28 22:33:01 -0500

    Fix template project's `use` call.

--- src/leiningen/new/template/temp.clj
+++ src/leiningen/new/template/temp.clj
@@ -1,5 +1,5 @@
 (ns leiningen.new.{{name}}
-  (:use leiningen.new.templates))
+  (:use [leiningen.new.templates :only [renderer name-to-path ->files]]))
 
 (def render (renderer "{{name}}"))
 
@@ -7,6 +7,6 @@
   "FIXME: write documentation"
   [name]
   (let [data {:name name
-              :sanitized (sanitize name)}]
+              :sanitized (name-to-path name)}]
     (->files data
              ["src/{{placeholder}}/foo.clj" (render "foo.clj" data)])))
