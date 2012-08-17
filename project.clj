184c5eaff78cc9167e219347f31eb9da06f1678b
Phil Hagelberg <technomancy@gmail.com>
2012-08-17 15:49:24 -0700

    Latest versions of stencil and bultitude.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-newnew "0.3.5-SNAPSHOT"
   :description "A Leiningen plugin for generating new projects based on templates."
-  :dependencies [[stencil "0.2.0"]
-                 [bultitude "0.1.5"]]
+  :dependencies [[stencil "0.3.0"]
+                 [bultitude "0.1.7"]]
   :eval-in-leiningen true)
