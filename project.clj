b2770a7a33251dafac0a6a922db9c10296aa6411
Phil Hagelberg <technomancy@gmail.com>
2012-08-17 17:08:14 -0700

    Aw heck; add in more metadata.

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.3.0"]
                  [bultitude "0.1.7"]]
+  :url "https://github.com/technomancy/leiningen"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
