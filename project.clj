2cae0b2eecf6df859a718aba663090f1a7f03a3e
Phil Hagelberg <technomancy@gmail.com>
2012-08-17 17:02:15 -0700

    Release 0.3.5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-newnew "0.3.5-SNAPSHOT"
+(defproject lein-newnew "0.3.5"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.3.0"]
                  [bultitude "0.1.7"]]
