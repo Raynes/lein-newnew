c27d13a0d75661eb505982f2a147cf9486886084
Phil Hagelberg <technomancy@gmail.com>
2012-08-17 17:09:33 -0700

    That's, like, not even the right URL.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.3.0"]
                  [bultitude "0.1.7"]]
-  :url "https://github.com/technomancy/leiningen"
+  :url "https://github.com/Raynes/lein-newnew"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
