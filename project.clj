083e03f4468344c69df2aa5d74f0ac65925fed32
Phil Hagelberg <technomancy@gmail.com>
2012-12-14 14:37:14 -0800

    Bump to 0.3.6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-newnew "0.3.5"
+(defproject lein-newnew "0.3.6"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.3.0"]
                  [bultitude "0.1.7"]]
